5b3434a71f4f5bd479723de0b353a723558180e7
Terence Stone <terence.stone1@gmail.com>
2016-10-24 19:28:26 -0400

    Revise ajax calls, and data flow for pokemon search display

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -3,56 +3,32 @@ class App extends React.Component {
     super()
     this.state = {
       pokemon: [],
-      descriptions: ""
+      sprites: [],
+      descriptions: []
     }
     this.addPokemon = this.addPokemon.bind(this)
-    // this.addDescription = this.addDescription.bind(this)
+    this.addDescription = this.addDescription.bind(this)
   }
-
-  // componentDidMount() {
-  //   // debugger
-  //   let pkmn = this.state.pokemon[0].name
-  //    $.ajax({
-  //       url: `http://pokeapi.co/api/v2/pokemon-species/${pkmn}/`,
-  //       method: 'get'
-  //     })
-  //     .done((response) => {
-  //       this.setState({
-  //         descriptions: response.flavor_text_entries[1].flavor_text
-  //       })
-  //     })
-  // }
 
   addPokemon(newPkmn) {
     this.setState({
-      pokemon: [newPkmn].concat(this.state.pokemon)
+      pokemon: [newPkmn.name].concat(this.state.pokemon),
+      sprites: [newPkmn.sprite].concat(this.state.sprites)
     })
-
-    let pkmn = this.state.pokemon[0].name
-     $.ajax({
-        url: `http://pokeapi.co/api/v2/pokemon-species/${pkmn}/`,
-        method: 'get'
-      })
-      .done((response) => {
-        this.setState({
-          descriptions: response.flavor_text_entries[1].flavor_text
-        })
-      })
   }
 
-  // addDescription(description) {
-    // debugger
-    // this.setState({
-    //   descriptions: description
-    // })
-    // // debugger
-  // }
+  addDescription(description) {
+    this.setState({
+      descriptions: [description].concat(this.state.descriptions)
+    })
+  }
+
 
   render() {
     return (
       <div className="container">
         <SearchBar onSearch={this.addPokemon} descript={this.addDescription}/>
-        <PokemonView pokemon={this.state.pokemon} descript={this.state.descriptions}/>
+        <PokemonView pokemon={this.state.pokemon} sprite={this.state.sprites} descript={this.state.descriptions}/>
       </div>
     )
   }
--- app/assets/javascripts/components/pokemonView.es6.jsx
+++ app/assets/javascripts/components/pokemonView.es6.jsx
@@ -6,7 +6,7 @@ class PokemonView extends React.Component {
       <div id="all-pokes">
         {
           this.props.pokemon.map((pkmn, i) => {
-            return (<Pokemon key={i} data={pkmn} descript={this.props.descript}/>)
+            return (<Pokemon key={i} name={pkmn} sprite={this.props.sprite[i]} descript={this.props.descript[i]}/>)
           })
         }
       </div>
